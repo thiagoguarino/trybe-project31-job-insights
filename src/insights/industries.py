b760fcb0dbfdcea6ee8b8fde21cbd3dae2edd495
Thiago Guarino <thiago.guarino@outlook.com>
2023-01-23 16:03:35 -0300

    refactor: function filter_by_industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -44,4 +44,10 @@ def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     list
         List of jobs with provided industry
     """
-    raise NotImplementedError
+    filtered_jobs_list = list()
+
+    for job in jobs:
+        if job["industry"] == industry:
+            filtered_jobs_list.append(job)
+
+    return filtered_jobs_list
